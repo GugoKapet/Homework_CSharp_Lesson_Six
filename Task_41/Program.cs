454245023330bff520f3bac8437ab9285356578d
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-11-26 22:48:57 +0200

    Added Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Insert number:");
+int numberOfFigures = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[numberOfFigures];
+int result = PositiveNumbersCount(array);
+Console.WriteLine(result);
+
+int PositiveNumbersCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
