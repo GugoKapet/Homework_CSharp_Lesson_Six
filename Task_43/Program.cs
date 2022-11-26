46f56b9999cc042f79f3451902f59fe2da04acb7
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-11-27 00:11:58 +0200

    Added Task 43

---
+++ Task_43/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("Insert points of coordiante:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+CrossingPoint(b1, k1, b2, k2);
+
+void CrossingPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+    Console.WriteLine($"Crossing point has following coordinates: {x}, {y}");
+}
